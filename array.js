b557f8321a69011d09cb8c216a6cedfd90ff01ec
lsasikala <lavanya.sudha27@gmail.com>
2023-05-17 16:44:39 -0400

    array file added

---
+++ array.js
@@ -0,0 +1,21 @@
+var a = [1,2,3,4]
+console.log(typeof(a))
+console.log(a[0])
+
+console.log(a[1])
+
+console.log(a[4])
+
+a[5]=500
+console.log(a[5])
+console.log(a)
+
+
+a.push(600)  //will be added as the last element.
+console.log(a)
+
+
+console.log(a.length)
+
+
+VAR B = NEW Array('A','B,'C')
